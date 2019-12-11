5e89f722d3f2b9a8e21776b1e400db62aa4c5277
taslimy <taslimer@gmail.com>
2019-12-11 17:42:24 -0500

    A basic template of a graphQL setup / schema

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import { GraphQLServer } from "graphql-yoga";
+
+// Type definitions (schema)
+
+// the useage of a ! means it will always return as a string
+const typeDefs = `
+type Query {
+  hello: String!,
+  name: String!,
+  location: String!,
+  bio: String!
+}
+`;
+
+// Resolvers
+
+const resolvers = {
+  Query: {
+    hello() {
+      return "Is this how it works?";
+    },
+    name() {
+      return "Tas Tas";
+    },
+    location() {
+      return "NYC BABY";
+    },
+    bio() {
+      return "lorem ipsum about me stuff ";
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+
+server.start(() => {
+  console.log(`The server is up!`);
+});
