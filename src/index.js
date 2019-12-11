bf21dfdcb046eb7c64d1607875a74e8c3a4bdcbc
taslimy <taslimer@gmail.com>
2019-12-11 17:26:55 -0500

    file clean up

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import add, { subtract } from "./math";
-
-console.log(add(5, 5));
-console.log(subtract(10, 5))
--- src/myModule.js
+++
@@ -1,5 +0,0 @@
-const message = "some message from this moudle.js LUL XD";
-
-const name = "tas"; 
-
-export { message, name };
